a1ef5bcf4fe7f34e1118fb4055b5806a0ed6174e
vinodpachipala1 <vinodpachipala93@gmail.com>
2026-01-27 21:59:56 +0530

    base url fix

--- frontend/src/components/Base_url.jsx
+++ frontend/src/components/Base_url.jsx
@@ -1,2 +1,4 @@
-const Base_url = "http://localhost:3001/api";
+// const Base_url = "http://localhost:3001/api"; <-- Old Local URL
+const Base_url = "https://vinod93-postathon.hf.space/api"; // <-- New Live URL
+
 export default Base_url;
