8c70839fdf0d86f5a2dc3271d12ced06ec27e24f
VINOD PACHIPALA <vinodpachipala93@gmail.com>
2026-02-27 12:24:24 +0530

    Update Base_url.jsx

--- frontend/src/components/Base_url.jsx
+++ frontend/src/components/Base_url.jsx
@@ -1,4 +1,4 @@
 // const Base_url = "http://localhost:3001/api";
-const Base_url = "https://vinod93-postathon.hf.space/api";
+const Base_url = "https://hackthon-postathon.onrender.com";
 
 export default Base_url;
